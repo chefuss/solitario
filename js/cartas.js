7e6e63e7af40dc53c8cb442cb966ddd0627c733c
ricardo genaizir <ricardogenaizir1@gmail.com>
2019-12-18 20:30:41 -0300

    terminado agregar cartas

--- js/cartas.js
+++ js/cartas.js
@@ -1,6 +1,24 @@
 // TODO crear función constructora de Carta
 
 // TODO crear función en el prototipo de Carta de crearCartas.
+Carta.prototype.crearCartas = function(array,selector) {
+      array.forEach(element => 
+      {
+        var boton = document.createElement('button')
+        boton.classList.add('carta');
+
+      boton.setAttribute('carta-numero',element.valor);
+    boton.setAttribute('carta-palo',element.palo);
+      boton.setAttribute('carta-color',element.color);
+
+       boton.innerHTML='<span>' + element.valor + '</span>';
+       boton.classList.add(element.palo);
+       boton.classList.add(element.color);
+
+       funcionesGenerales.agregar(document.querySelector(selector),'is-visible');
+       document.querySelector(selector).append(boton);
+      });
+    }
 
 Carta.prototype.obtenerCarta = function(carta) {
     var numero = carta.getAttribute('carta-numero');
